815e3ebff30408da4d536eef1b98af2ebc3d819c
Rawand Dargali <rdargali@yahoo.com>
2020-04-14 23:09:31 -0500

    updated card hover

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -3,8 +3,10 @@ import "../App.css";
 const NavButtons = (props) => {
   const buttonStyle = {
     color: "yellow",
-
+    borderRadius: "50px",
     width: "10rem",
+    margin: "0 2rem",
+    marginTop: "1rem",
     // fontSize: "25px",
   };
 
@@ -12,21 +14,21 @@ const NavButtons = (props) => {
     <div className="button-container">
       {props.goToPrevPage && (
         <button
-          className="btn btn-primary btn-lg"
+          className="btn btn-primary"
           style={buttonStyle}
           onClick={props.goToPrevPage}
         >
-          <i class="fas fa-arrow-circle-left" />
+          <i class="fas fa-arrow-circle-left display-4" />
         </button>
       )}
 
       {props.goToNextPage && (
         <button
-          className="btn btn-primary btn-lg"
+          className="btn btn-primary"
           style={buttonStyle}
           onClick={props.goToNextPage}
         >
-          <i class="fas fa-arrow-circle-right" />
+          <i class="fas fa-arrow-circle-right display-4" />
         </button>
       )}
     </div>
