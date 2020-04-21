87895d98e19c0e2510bba65630250ca268ae90b9
Rawand Dargali <rdargali@yahoo.com>
2020-04-21 00:55:12 -0500

    populating pokemon page

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../App.css";
 
 import axios from "axios";
 
@@ -17,12 +18,22 @@ const Pokemon = (props) => {
   //   console.log(pokemon.sprites);
 
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <img
-        src={pokemon.sprites ? pokemon.sprites.front_default : null}
-        alt={pokemon.name}
-      />
+    <div className="display 4">
+      <h1>
+        {pokemon.name &&
+          pokemon.name.charAt(0).toUpperCase() + pokemon.name.substring(1)}
+      </h1>
+      <div className="sprites">
+        <img
+          src={pokemon.sprites && pokemon.sprites.front_default}
+          // src={pokemon.sprites ? pokemon.sprites.front_default : null}
+          alt={pokemon.name}
+        />
+        <img
+          src={pokemon.sprites && pokemon.sprites.back_default}
+          alt={pokemon.name}
+        />
+      </div>
     </div>
   );
 };
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../App.css";
 
@@ -12,20 +13,27 @@ const PokemonList = (props) => {
   return (
     <div className="pl-container">
       {props.pokemon.map((pokemon) => (
-        <div key={pokemon.name} className="card bg-light m-3" style={cardStyle}>
-          <div>
-            <img
-              className="card-img-top"
-              src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
-              alt={`${pokemon.name}`}
-            />
-            <div className="card-title text-center">
-              <strong>
-                {pokemon.name.charAt(0).toUpperCase() +
-                  pokemon.name.substring(1)}
-              </strong>
+        <div
+          key={pokemon.name}
+          className="card bg-light m-3"
+          style={cardStyle}
+          to="/bulbasaur"
+        >
+          <Link to={`/${pokemon.name}`}>
+            <div>
+              <img
+                className="card-img-top"
+                src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
+                alt={`${pokemon.name}`}
+              />
+              <div className="card-title text-center">
+                <strong>
+                  {pokemon.name.charAt(0).toUpperCase() +
+                    pokemon.name.substring(1)}
+                </strong>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       ))}
     </div>
